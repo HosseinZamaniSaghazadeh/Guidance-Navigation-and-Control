4c000443545d70e92f153e50d3ddb37834bb83b5
Hossein Zamani Saghazadeh <hosseinzamani1995@gmail.com>
2025-05-25 13:52:36 -1000

    Modified

--- Optimal Control/Golden_Section/Golden_Section.m
+++ Optimal Control/Golden_Section/Golden_Section.m
@@ -3,8 +3,10 @@
 %% Parameters
  xL = 0;
  xU = 4;
- N = 100;
+ N = 20;
 phi = (1 + sqrt(5))/2;
+x1 = zeros(N,1);
+x2 = zeros(N,1);
  %% Funtion to be Omptimized
  f = @(x) x^2/10 - 2*sin(x);
 
@@ -12,25 +14,33 @@ phi = (1 + sqrt(5))/2;
  for i = 1:N
 
     d = (phi -1)*(xU - xL);
-    x1 = xL + d;
-    x2 = xU - d;
+    x1(i) = xL + d;
+    x2(i) = xU - d;
 
-    if f(x1) < f(x2)
+    if f(x1(i)) < f(x2(i))
 
-        xL = x2;
+        xL = x2(i);
 
-    elseif f(x1) > f(x2)
+    elseif f(x1(i)) > f(x2(i))
 
-        xU = x1;
+        xU = x1(i);
 
     else
 
-        xL = x2;
-        xU = x1;
+        xL = x2(i);
+        xU = x1(i);
 
     end
 
  end
 
- disp(x1)
- disp(x2)
+ disp(x1(end))
+ disp(x2(end))
+
+ %% Plot Convergence
+
+ plot(1:N, x1, 1:N, x2);
+ grid on
+ xlabel('Iteration');
+ ylabel('X1,2');
+ title('Golden Section Optimization');
